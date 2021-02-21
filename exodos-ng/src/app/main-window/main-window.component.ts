5fadd430dae37d73fbe1e461e81fea1fc9532c5c
tizenegy <bastian.elfert@outlook.com>
2021-02-21 16:15:37 +0100

    removed img in main screen, added canvas and button, created drawing and animation.

--- exodos-ng/src/app/main-window/main-window.component.ts
+++ exodos-ng/src/app/main-window/main-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main-window',
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-window.component.css']
 })
 export class MainWindowComponent implements OnInit {
+  @ViewChild('canvas', { static: true }) public canvas!: ElementRef;
+  private ctx!: CanvasRenderingContext2D;
+  squares: Square[] = new Array();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
   }
 
+  animate(): void {
+    this.ctx.fillStyle = 'green';
+    const square = new Square(this.ctx);
+    this.squares.push(square);
+    console.log(this.squares);
+    square.move(this.squares);
+  }
+}
+
+export class Square {
+  private color = 'green';
+  private x = 0;
+  private y = 0;
+  private z = 30;
+
+  constructor(private ctx: CanvasRenderingContext2D) {}
+
+  moveRight() {
+    this.x++;
+    this.draw();
+  }
+
+  private draw() {
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.z * this.x, this.z * this.y, this.z, this.z);
+  }
+
+  move(squares:Square[]) {
+    const width = this.ctx.canvas.width;
+    const height = this.ctx.canvas.height;
+    setInterval(() => {
+      this.ctx.clearRect(0, 0, width, height);
+      squares.forEach((square: Square) => {
+        square.moveRight();
+      });
+    }, 2000);    
+  }
 }
